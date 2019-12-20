536982a26217e658f0ce4b521862b499e6332781
Leonardo Benassi <leonardobenassi.97@libero.it>
2019-12-20 22:43:41 +0100

    dependencies updated

--- city_tour_planner/src/App.js
+++ city_tour_planner/src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import MainFrame from './components/main/MainFrame';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      
+      <MainFrame/>
+    
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -18,7 +22,8 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+    
     </div>
   );
 }
